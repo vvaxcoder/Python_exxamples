781e21b113fe70ed80218ce9a182b7cae5a399ac
VVoronkov <voronglass84@gmail.com>
2025-01-26 12:39:02 +0300

    Update nester.py with use class attributes

--- nester.py
+++ nester.py
@@ -2,9 +2,9 @@ def generate():                  # Терпит неудачу при выпол
     class Spam:                  # Python до версии 2.2
         count = 1
 
-        def method(self):        # Имя Spam недоступно:
-            print(Spam.count)    # Не локальное (def), не глобальное (модуль),
-                                 # не встроенное
-        return Spam()
+        def method(self):                  # Имя Spam недоступно:
+            # print(Spam.count)            # Не локальное (def), не глобальное (модуль), не встроенное
+            print(self.__class__.count)    # Работает: используется атрибут для получения класса
+    return Spam()
 
 generate().method()
